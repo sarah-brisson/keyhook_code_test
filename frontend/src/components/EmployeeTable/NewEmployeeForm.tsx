4d0b4670a4dd58ce733baaf2fc3f5645436942f6
sarah <sar.brisson@gmail.com>
2025-04-07 14:53:23 +1200

    Frontend - Add validation to the employee creation form

--- frontend/src/components/EmployeeTable/NewEmployeeForm.tsx
+++ frontend/src/components/EmployeeTable/NewEmployeeForm.tsx
@@ -3,6 +3,7 @@ import { Modal, Button, Form, Input, InputNumber } from 'antd';
 import DepartmentSelect from './DeparmentSelect';
 import { DepartmentOption } from '../../utils/types';
 import { Employees } from '../../api/apiConfig';
+import { capitalizeFirstLetter } from '../../utils/textTreatment';
 
 const NewEmployeeForm: React.FC = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
@@ -47,6 +48,9 @@ const NewEmployeeForm: React.FC = () => {
             .then((values) => {
                 const newEmployeeData = {
                     ...values,
+                    first_name: capitalizeFirstLetter(values.first_name),
+                    last_name: capitalizeFirstLetter(values.last_name),
+                    position: capitalizeFirstLetter(values.position),
                     department_id: selectedDepartment,
                 };
 
@@ -56,8 +60,7 @@ const NewEmployeeForm: React.FC = () => {
                 }
 
                 createNewEmployee(newEmployeeData);
-                // setIsModalVisible(false);
-                // form.resetFields();
+                form.resetFields();
             })
             .catch((info) => {
                 console.error('Validation Failed:', info);
@@ -82,14 +85,20 @@ const NewEmployeeForm: React.FC = () => {
                     <Form.Item
                         label="First Name"
                         name="first_name"
-                        rules={[{ required: true, message: 'Please input the first name!' }]}
+                        rules={[
+                            { required: true, message: 'Please input the first name!' },
+                            { pattern: /^[a-zA-Z\s]*$/, message: 'First name must not contain extra characters' },
+                        ]}
                     >
                         <Input placeholder="Enter first name" />
                     </Form.Item>
                     <Form.Item
                         label="Last Name"
                         name="last_name"
-                        rules={[{ required: true, message: 'Please input the last name!' }]}
+                        rules={[
+                            { required: true, message: 'Please input the last name!' },
+                            { pattern: /^[a-zA-Z\s]*$/, message: 'Last name must not contain extra characters' },
+                        ]}
                     >
                         <Input placeholder="Enter last name" />
                     </Form.Item>
@@ -98,7 +107,7 @@ const NewEmployeeForm: React.FC = () => {
                         name="age"
                         rules={[
                             { required: true, message: 'Please input the age!' },
-                            { type: 'number', min: 0, message: 'Age must be a positive number!' },
+                            { type: 'number', min: 15, max: 100, message: 'Must be a reasonable age' },
                         ]}
                     >
                         <InputNumber placeholder="Enter Age" />
@@ -106,16 +115,13 @@ const NewEmployeeForm: React.FC = () => {
                     <Form.Item
                         label="Position"
                         name="position"
-                        rules={[{ required: true, message: 'Please input the position!' }]}
+                        rules={[
+                            { required: true, message: 'Please input the position!' },
+                        ]}
                     >
                         <Input placeholder="Enter position" />
                     </Form.Item>
                     <DepartmentSelect selectDepartment={handleDepartmentSelect} />
-                    {/* <Form.Item
-                        label="Department"
-                        name="department"
-                    >
-                    </Form.Item> */}
                 </Form>
             </Modal>
         </>
